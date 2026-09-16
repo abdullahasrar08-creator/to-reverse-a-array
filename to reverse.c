b1970aba27a6f4ea881586d2df9ea00c5cd45524
Abdullah Israr Ahmed Bubere <abdullahasrar08@gmail.com>
2026-09-16 21:33:37 +0530

    Add files via upload

---
+++ to reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\nArray in reversed order:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
